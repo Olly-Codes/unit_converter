0d1f71961034694a90a3ec2227cb2ab236f16afe
Olwethu Ngcobo <nolwethu70@gmail.com>
2025-07-29 01:27:33 +0200

    Added templates and pages

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, redirect, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/weight")
+def weight():
+    return render_template("weight.html")
+
+@app.route("/temperature")
+def temperature():
+    return render_template("temperature.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
