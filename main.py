2b04ff62812db06496d1e459700b4cabd36036ab
Olwethu Ngcobo <nolwethu70@gmail.com>
2025-08-02 01:24:36 +0200

    Added length template, Added redirects

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, url_for, request
 
 app = Flask(__name__)
 
@@ -7,23 +7,44 @@ def index():
     if request.method == "POST":
 
         try:
-            category = request.form["category"]
-            value = float(request.form["value"])
-            from_unit = request.form["from_unit"]
-            to_unit = request.form["to_unit"]
+            category: str = request.form["category"]
+            value: float = float(request.form["value"])
+            from_unit: str = request.form["from_unit"]
+            to_unit: str = request.form["to_unit"]
 
             print(category, value, from_unit, to_unit)
+
+            match category:
+                case "length":
+                    return redirect(url_for("length", length=value,
+                                             from_unit=from_unit,
+                                                to_unit=to_unit))
+                case "weight":
+                    return redirect(url_for("weight", weight=value,
+                                             from_unit=from_unit,
+                                                to_unit=to_unit))
+                case "temperature":
+                    return redirect(url_for("temperature", temp=value,
+                                             from_unit=from_unit,
+                                                to_unit=to_unit))
         except Exception as e:
             print(e)
-        return f"It works"
     return render_template("index.html")
 
-@app.route("/weight")
-def weight():
+@app.route("/length/<float:length>/<from_unit>/<to_unit>")
+def length(length: float, from_unit: str, to_unit: str):
+    print(f"You made it! {length}{from_unit}{to_unit}")
+    return render_template("length.html")
+
+
+@app.route("/weight/<float:weight>/<from_unit>/<to_unit>")
+def weight(weight: float, from_unit: str, to_unit: str):
+    print(f"You made it! {weight}{from_unit}{to_unit}")
     return render_template("weight.html")
 
-@app.route("/temperature")
-def temperature():
+@app.route("/temperature/<float:temp>/<from_unit>/<to_unit>")
+def temperature(temp: float, from_unit: str, to_unit: str):
+    print(f"You made it! {temp}{from_unit}{to_unit}")
     return render_template("temperature.html")
 
 if __name__ == "__main__":
